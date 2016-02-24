d1c8fa6ecf161baeaffaf6cad6a16bd628024536
JulissaDantes <julissadcj@gmail.com>
2016-02-24 18:09:42 -0430

    Update app.js
    Ya

--- src/app.js
+++ src/app.js
@@ -1,6 +1,56 @@
 
+
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
+    turn: 0,
+    size: null,
+    p1s: [],//almacena las x
+    p2s: [],//almacena las o
+    ,
+	random: function getRandomInt(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	},
+    creaX: function(){
+		if(turn == 0||turn == 2||turn == 4||turn == 6||turn == 8)
+            {
+		var p1 = new cc.Sprite(res.X_png);
+		p1.setScale(0.4,0.4);
+        p1.setPosition(this.random(1,480), this.size.height );
+        this.addChild(p1, 1);
+		var moveto = cc.moveTo(this.random(1,9), this.p1.getPositionX(), this.p1.getPositionY());
+		p1.runAction(moveto);
+		this.p1s.push(p1);
+        turn = turn + 1;//para saber a quien le toca jugar
+            }
+    },
+    
+    creaO: function(){
+		if(turn == 1||turn == 3||turn == 5||turn == 7||turn == 9)
+            {
+		var p2 = new cc.Sprite(res.O_png);
+		p2.setScale(0.4,0.4);
+        p2.setPosition(this.random(1,480), this.size.height );
+        this.addChild(p2, 1);
+		var moveto = cc.moveTo(this.random(1,9), this.p2.getPositionX(), this.p2.getPositionY());
+		p2.runAction(moveto);
+		this.p2s.push(p2);
+         turn = turn + 1;
+            }
+    },
+    Ganar: function(location, event){
+		var ubicacion = location.getLocation();
+		var juego = event.getCurrentTarget();
+		for(var p1 of juego.p1s){
+			var cuadro = p1.getBoundingBox();
+			if(cc.rectContainsPoint(cuadro,ubicacion)){
+				
+			}
+			
+		}
+		return true;
+		
+	},
+    
     ctor:function () {
         //////////////////////////////
         // 1. super init first
@@ -31,6 +81,14 @@ var HelloWorldLayer = cc.Layer.extend({
         });
         this.addChild(this.sprite, 0);
 
+
+//Inicializando eventos
+		cc.eventManager.addListener({
+			event: cc.EventListener.TOUCH_ONE_BY_ONE,
+			onTouchBegan: this.creaX,
+			onTouchBegan: this.creaO
+			
+		}, this);
         return true;
     }
 });
